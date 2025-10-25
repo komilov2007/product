8999fc5c9af9500417acd6c9da622ece56730c86
Rahmatulloh <komilov2007.2007@gmailcom>
2025-10-25 11:09:03 +0500

    added

--- main2.js
+++ main2.js
@@ -1,10 +1,14 @@
 const yosh = +prompt('Yoshingizni kiriting?');
 if (yosh >= 18) {
-  alert('Siz ushbu web sahifaga kirishingiz mumkin!!!', product());
+  alert('Siz ushbu web sahifaga kirishingiz mumkin!!!');
+  product();
 } else {
   alert('Afsuski siz 18 yoshdan kichkinasiz bu sahifaga kira olmaysiz!!!');
-  alert.style.backgroundColor = 'purple';
+  document.body.style.backgroundColor = 'black';
+  document.body.innerHTML =
+    '<p class="yopiq" style="color: white;">Afsuski bu sahifa siz uchun yopiqda!!! </p><p class="yopiq">Chunki siz 18 yoshga to\'lmagansiz</p>';
 }
+
 const div = document.querySelector('.products');
 const load = document.querySelector('.loader');
 
@@ -12,16 +16,27 @@ async function product() {
   try {
     const res = await fetch('https://fakestoreapi.com/products');
     const data = await res.json();
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    await new Promise((resolve) => setTimeout(resolve, 1500));
     load.style.display = 'none';
+
     data.forEach((item) => {
-      div.innerHTML += `<div class="card">
-                 <img src="${item.image}" alt="${item.title}">
-           <h3>${item.title}</h3>
-         <p>${item.price}$</p>
-         </div>`;
+      const card = document.createElement('div');
+      card.classList.add('card');
+      card.innerHTML = `
+        <img src="${item.image}" alt="${item.title}">
+        <h3>${item.title}</h3>
+        <p>${item.price}$</p>
+      `;
+
+      card.addEventListener('click', () => {
+        card.style.backgroundColor = 'black';
+        card.style.color = 'white';
+        card.style.transition = '0.3s';
+      });
+
+      div.appendChild(card);
     });
   } catch (error) {
-    load.innerHTML = ``;
+    load.innerHTML = `<p>Xatolik yuz berdi!</p>`;
   }
 }
